ed8a953ec36dc4ae5b05ee1bb632c3acf87b3f52
Camilo Enriquez <109879911+camiloenriquez18@users.noreply.github.com>
2023-03-14 16:18:24 -0300

    segunda pre entrega

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
+class Poliza {
+    constructor(nombredelasegurado, patente, anio, marca, modelo, id) {
+    this.nombredelasegurado = nombredelasegurado;
+    this.patente = patente;
+    this.anio = anio;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.id = id;
+    }
+    
+    static ordenarPorId(polizas) {
+        return polizas.sort((a, b) => a.id - b.id);
+    }
+    asignarId(polizas) {
+    this.id = polizas.length;
+    }
+}
+
+const arraydepolizas = {
+    elementos_polizas: [],
+    agregarPoliza: function(elemento) {
+    this.elementos_polizas.push(elemento);
+    }
+};
+
 
 function menuInicio() {
 let opcion = prompt(
-    "Bienvenido al Simulador de Seguros\n\nPor favor seleccione una opción:\n1. Seguro de Auto\n2. Seguro de Hogar\n3. Salir"
+    "Bienvenido al Simulador de Seguros\n\nPor favor seleccione una opción:\n1. Calcular seguro de vehiculo. \n2. Registrar vehiculo. \n3. Ver los vehiculos asegurados."
     );
 
     switch (opcion) {
     case "1":
-        calcularSeguroAuto();
+        calcularSeguroVehiculo();
         break;
     case "2":
-        calcularSeguroHogar();
+        registrarvehiculo();
         break;
     case "3":
-        alert("Gracias por usar el Simulador de Seguros.");
+        verlosvehiculosasegurados()
         break;
     default:
         alert("Opción inválida. Por favor seleccione una opción válida.");
@@ -21,27 +46,14 @@ let opcion = prompt(
     }
 }
 
-
-function calcularSeguroAuto() {
+function calcularSeguroVehiculo() {
     let edad = prompt("Por favor ingrese su edad:");
-    let modelo = prompt("Por favor ingrese el modelo de su auto:");
+    let modelo = prompt("Por favor ingrese el modelo de su vehiculo:");
     let monto = prompt("Por favor ingrese el monto de cobertura deseado:");
 
 
 
-    alert("El monto mensual para su seguro de auto es: " + Math. round(( edad *2 +  modelo * 0,02 + monto/90 )));
-    volverMenu();
-}
-
-
-function calcularSeguroHogar() {
-    let antiguedad = prompt("Por favor ingrese la antiguedad de su residencia expresado en años:");
-    let valor = prompt("Por favor ingrese el valor de su hogar:");
-    let monto = prompt("Por favor ingrese el monto de cobertura deseado:");
-
-
-
-    alert("El monto mensual para su seguro de hogar es: " + Math. round((antiguedad * 230  + monto /620  )));
+    alert("El monto mensual para su seguro de vehiculo es: " + Math. round(( edad *2 +  modelo * 0,02 + monto/90 )));
     volverMenu();
 }
 
@@ -65,3 +77,31 @@ function volverMenu() {
 
 
 menuInicio();
+
+
+function registrarvehiculo(){
+let ingreso = prompt('Ingresa los datos separados por "\\" (nombre, patente, año, marca, modelo, id)');
+let datos = ingreso.split('\\');
+const poliza = new Poliza(datos[0], datos[1], parseInt(datos[2]), datos[3], datos[4], parseInt(datos[5]));
+arraydepolizas.agregarPoliza(poliza);
+console.log(arraydepolizas.elementos_polizas);
+alert("registrado con exito")
+menuInicio();
+}
+
+
+function verlosvehiculosasegurados() {
+    if (arraydepolizas.elementos_polizas.length === 0) {
+        alert("No hay vehículos asegurados.");
+        menuInicio();
+    } else {
+        Poliza.ordenarPorId(arraydepolizas.elementos_polizas);
+        let polizasRegistradas = "Lista de vehículos asegurados:\n\n";
+        for (let i = 0; i < arraydepolizas.elementos_polizas.length; i++) {
+            let polizaActual = arraydepolizas.elementos_polizas[i];
+            polizasRegistradas += `Nombre del asegurado: ${polizaActual.nombredelasegurado}\nPatente: ${polizaActual.patente}\nAño: ${polizaActual.anio}\nMarca: ${polizaActual.marca}\nModelo: ${polizaActual.modelo}\nID: ${polizaActual.id}\n\n`;
+        }
+        alert(polizasRegistradas);
+        menuInicio();
+    }
+}
